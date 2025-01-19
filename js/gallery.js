fd2680d690d157d1fed4f230bd1607cc33b7143d
SrNe1103 <vgarridof@uc.cl>
2025-01-19 17:31:09 -0300

    update gallery styles and structure

--- js/gallery.js
+++ js/gallery.js
@@ -2,13 +2,14 @@ function upDate(preview){
     pic_frame = document.querySelector('#image');
     pic_frame.style.backgroundImage = "url('" + preview.src + "')";
     pic_frame.innerHTML = preview.alt;
-    pic_frame.style.height = preview.height * 575 / preview.width + "px";
+    pic_frame.style.height = preview.height * 500 / preview.widt + "px";
 }
    
 function unDo(){
     pic_frame = document.querySelector('#image');
     pic_frame.style.backgroundImage = "";
     pic_frame.innerHTML = "Hover over or select an image below to display here."; 
+    pic_frame.style.height = "620px";
 }
 
 let images = document.querySelectorAll('.preview');
